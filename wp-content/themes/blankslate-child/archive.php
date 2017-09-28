39ff867cc6ff9907de19a34e72d786d5e0a444f2
Shawn Flock <sflock@navicamls.net>
2017-09-28 13:58:15 -0400

    kjdsxzh

--- wp-content/themes/blankslate-child/archive.php
+++ wp-content/themes/blankslate-child/archive.php
@@ -18,9 +18,16 @@
     <?php 
         if ( have_posts() ) : while ( have_posts() ) : the_post();
     ?>
-    <?php get_template_part( 'entry' ); ?>
-    <?php endwhile; endif; ?>
-    <?php get_template_part( 'nav', 'below' ); ?>
+    <?php
+         get_template_part( 'entry' ); 
+    ?>
+    <?php 
+        endwhile; 
+        endif; 
+    ?>
+    <?php 
+        get_template_part( 'nav', 'below' ); 
+    ?>
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
