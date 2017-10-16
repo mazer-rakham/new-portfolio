d94216d769422d4a4b098f12fcdff5a4a4aa3483
Shawn Flock <sflock@navicamls.net>
2017-10-16 12:58:49 -0400

    adding some stuff

--- wp-content/themes/blankslate-child/main-page.php
+++ wp-content/themes/blankslate-child/main-page.php
@@ -24,8 +24,39 @@ get_header('secondary'); ?>
         ?>
     </div>
     <!-- <div id="site-description"><?php bloginfo( 'description' ); ?></div>  -->
-    <h1>My website is as sparadic as my mind</h1>
+    <h1>My website is as sporadic as my mind</h1>
 </section>
+<section id="content" role="main">
+        <?php 
+            if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="header">
+                <h1 class="entry-title">
+                    <?php 
+                        the_title(); 
+                    ?>
+                </h1> 
+                <?php 
+                    edit_post_link(); 
+                ?>
+            </header>
+            <section class="entry-content">
+                <?php 
+                    if ( has_post_thumbnail() ) { 
+                        the_post_thumbnail(); 
+                    }                
+                    the_content(); 
+                ?>
+                <div class="entry-links">
+                    <?php 
+                        wp_link_pages(); 
+                    ?>
+                </div>
+            </section>
+        </article>
+    <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+    <?php endwhile; endif; ?>
 <?php
 get_sidebar();
 get_footer(); ?>
